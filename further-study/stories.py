cda2dcebac957605de5dcf7c12bcf6265fd311f7
asearer <amsearer84@gmail.com>
2023-08-28 14:58:38 -0400

    On branch master  Initial commit  Changes to be committed: 	new file:   basic/app.py 	new file:   basic/requirements.txt 	new file:   basic/stories.py 	new file:   basic/templates/questions.html 	new file:   basic/templates/story.html 	new file:   further-study/app.py 	new file:   further-study/requirements.txt 	new file:   further-study/stories.py 	new file:   further-study/templates/questions.html 	new file:   further-study/templates/select-story.html 	new file:   further-study/templates/story.html

---
+++ further-study/stories.py
@@ -0,0 +1,76 @@
+"""Madlibs Stories."""
+
+
+class Story:
+    """Madlibs story.
+
+    To  make a story, pass a code, a title, a list of prompts,
+    and the text of the template.
+
+        >>> s = Story(
+        ...     "simple",
+        ...     "A Simple Tale",
+        ...     ["noun", "verb"],
+        ...     "I love to {verb} a good {noun}.")
+
+    To generate text from a story, pass in a dictionary-like thing
+    of {prompt: answer, promp:answer):
+
+        >>> ans = {"verb": "eat", "noun": "mango"}
+        >>> s.generate(ans)
+        'I love to eat a good mango.'
+    """
+
+    def __init__(self, code, title, words, text):
+        """Create story with words and template text."""
+
+        self.code = code
+        self.title = title
+        self.prompts = words
+        self.template = text
+
+    def generate(self, answers):
+        """Substitute answers into text."""
+
+        text = self.template
+
+        for (key, val) in answers.items():
+            text = text.replace("{" + key + "}", val)
+
+        return text
+
+
+# Here's a story to get you started
+
+
+story1 = Story(
+    "history",
+    "A History Tale",
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+story2 = Story(
+    "scifi",
+    "A SciFi Adventure",
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Along time ago in a far away {place}, suddenly a
+       large {adjective} {noun} was discovered. It was found 
+       to {verb} {plural_noun}."""
+)
+
+story3 = Story(
+    "western",
+    "A Western Story",
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Across the vast plains of {place}, suddenly a
+       large {adjective} {noun} was discovered. It was found 
+       to contain {verb} {plural_noun}."""
+)
+
+
+
+# Make dict of {code:story, code:story, ...}
+stories = {s.code: s for s in [story1, story2, story3]}
+
